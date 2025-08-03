4206055a48109ea9163d5f0d1e5a08c3f8d9c5bd
Isak Hauknes <isakhauknes@gmail.com>
2025-08-03 04:56:53 +0200

    Add vite config base targeting repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/colorblind-sim/', // ← Add this for GitHub Pages project site
   server: {
     port: 3000,
     open: true
   }
-}) 
+})
